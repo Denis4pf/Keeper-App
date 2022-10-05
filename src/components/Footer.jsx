4618aa1007190376cbb492f0cb1d731bfd5e428f
Denis4pf <denis.onwonga@student.moringaschool.com>
2022-10-06 01:37:17 +0300

    uploading the components code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+let d = new Date();
+let time = d.getFullYear();
+function Footer() {
+    return <>
+    <footer>
+        <p> Copywrite ⓒ {time}</p>
+    </footer>
+    </>
+}
+export default Footer;
